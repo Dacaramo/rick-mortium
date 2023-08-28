79cfeca578a8b7c965bd9c88193c4148d711c08d
Dacaramo <dankramirez@outlook.com>
2023-08-28 17:22:57 -0500

    feat: zustand and axios config

---
+++ src/model/Episode.ts
@@ -0,0 +1,9 @@
+export interface Episode {
+  id: string;
+  name: string;
+  airDate: string;
+  episode: string;
+  characters: Array<string>;
+  url: string;
+  created: string;
+}
---
+++ src/zustand/store.ts
@@ -0,0 +1,60 @@
+import { Character } from '../model/Character';
+import { Episode } from '../model/Episode';
+import { Location } from '../model/Location';
+import { create } from 'zustand';
+
+interface StoreState {
+  characters: Array<Character>;
+  locations: Array<Location>;
+  episodes: Array<Episode>;
+}
+
+export const useStore = create<StoreState>()((set) => {
+  return {
+    characters: [],
+    locations: [],
+    episodes: [],
+    saveCharacter: (character: Character) => {
+      return set((state) => {
+        return {
+          characters: [...state.characters, character],
+        };
+      });
+    },
+    saveLocation: (location: Location) => {
+      return set((state) => {
+        return {
+          locations: [...state.locations, location],
+        };
+      });
+    },
+    saveEpisode: (episode: Episode) => {
+      return set((state) => {
+        return {
+          episodes: [...state.episodes, episode],
+        };
+      });
+    },
+    removeSavedCharacter: (index: number) => {
+      return set((state) => {
+        return {
+          characters: state.characters.filter((_, i) => i !== index),
+        };
+      });
+    },
+    removeSavedLocation: (index: number) => {
+      return set((state) => {
+        return {
+          locations: state.locations.filter((_, i) => i !== index),
+        };
+      });
+    },
+    removeSavedEpisode: (index: number) => {
+      return set((state) => {
+        return {
+          episodes: state.episodes.filter((_, i) => i !== index),
+        };
+      });
+    },
+  };
+});
